802b55bc923c8ebdf7116d5ff91aa25d7dbf9271
hafijure <hafijure@gmail.com>
2023-04-05 23:42:28 +0600

    first commit

---
+++ isPrimeNumber.js
@@ -0,0 +1,20 @@
+// function isPrime(num) {
+//     if (num <= 1) {
+//       return false;
+//     }
+//     for (let i = 2; i <= Math.sqrt(num); i++) {
+//       if (num % i === 0) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// const givernNumber = 5;
+// const result = isPrime(givernNumber);
+// if (result == true) {
+//     console.log(givernNumber + ' is a prime number');
+// }else{
+//     console.log(givernNumber + ' is not a prime number');
+// }
+
+console.log(Math.sqrt(9));
---
+++ triangle-area.js
@@ -0,0 +1,13 @@
+function findagnlearea(side1,side2,side3){
+    const s = (side1 + side2 + side3)/2;
+    const area =  Math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
+    return area;
+}
+const side1 = 5; 
+const side2 = 6; 
+const side3 = 7; 
+const result = findagnlearea(side1,side2,side3);
+console.log(result);
+
+
+
