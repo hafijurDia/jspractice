71fcf47e4abb7bea82afd3ff36cb8159f00e7696
hafijure <hafijure@gmail.com>
2023-04-06 16:37:23 +0600

    remove duplicate

---
+++ removeDuplicate.js
@@ -0,0 +1,14 @@
+const friends = ['abul','kabul','abul','saleh','karim','rahim','sumon','abul','saleh','karim'];
+function removeDuplicate(){
+    const unique = [];
+    for (let i = 0; i < friends.length; i++) {
+        const name = friends[i];
+        console.log(name);
+        if (unique.includes(name) === false) {
+            unique.push(name);
+        }
+        
+    }
+    return unique;
+}
+console.log(removeDuplicate());
