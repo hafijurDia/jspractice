ccc5001dbf022e1168478d14f29a4b871403ad14
hafijure <hafijure@gmail.com>
2023-04-08 01:20:27 +0600

    biggest name string

--- paperRequirment.js
+++ paperRequirment.js
@@ -5,4 +5,19 @@ function paperRequirements(firstbook,secondbook,thirdbook){
     return firstBookTotalPage + secondtBookTotalPage  + thirdBookTotalPage
 }
 const totalbookpage =  paperRequirements(2,3,4);
-console.log('total book page: ' + totalbookpage);
+// console.log('total book page: ' + totalbookpage);
+
+// find a best friend
+function bestFriend(friendarray){
+    let biggest = '';
+    for (let i = 0; i < friendarray.length; i++) {
+        if (friendarray[i].length > biggest.length) {
+            biggest = friendarray[i];
+        }
+    }
+    return biggest;
+    
+}
+const friends = ["Alice", "Bob", "Charlie", "Dave", "Emily"];
+const result =  bestFriend(friends);
+console.log(result);
