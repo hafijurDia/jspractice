5ddd190bd215dee46a449ae0c67b543b99bfad4c
hafijure <hafijure@gmail.com>
2023-05-21 00:54:54 +0600

    country details by button done

--- js/countries.js
+++ js/countries.js
@@ -11,13 +11,31 @@ function displayCountry(countries){
         const singCountry = document.createElement('div');
         singCountry.classList.add('sing-country');
         singCountry.innerHTML = `
-       
         <p>Country Name:${country.name.common} </p>
-        <p>Capital Name: ${country.capitla ? country.capital : 'No Capital'}</p>
+        <p>Capital Name: ${country.capital ? country.capital : 'No Capital'}</p>
+        <img width="100px" src="${country.flags.png}" />
+        <button onclick="loadCountryDetails('${country.cca2}')">Details</button>
         `
         countryWrap.appendChild(singCountry);
-        console.log(country);
     });
+}
+
+
+const loadCountryDetails = (code) => {
+    const url = `https://restcountries.com/v3.1/alpha/${code}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetails(data[0]));
+}
+
+const displayCountryDetails = country => {
+    const detailsWrap = document.getElementById('show-details');
+    detailsWrap.innerHTML = `
+        <h3>Country: ${country.name.common} </h3>
+        <h5>Region: ${country.region}</h5>
+        <img src="${country.flags.png}" />
+    `
+    console.log(country)
 }
 
 loadCountry();
