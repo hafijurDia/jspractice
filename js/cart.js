3b43382d08af8f4526d5b0faf51a1cf07e2473a2
hafijure <hafijure@gmail.com>
2023-06-24 00:52:32 +0600

    practice of local storage done

--- js/cart.js
+++ js/cart.js
@@ -44,3 +44,60 @@ const addProduct = () => {
 
  }
  displayProductsFromLocalStorage();
+
+ //js prompt functinality
+ const promptFunction = () => {
+    let getValue = prompt("please enter number","0");
+    if (getValue != null) {
+       const valueParse = parseInt(getValue) + 200;
+       document.getElementById('prompt-result').innerText = valueParse;
+       
+    }
+ }
+
+ //js confirmed functionality
+ const confirmFunciton = () => {
+    let text = "Do you want to see website location?\nEither Confirm or Cancel.";
+    if (confirm(text) == true) {
+        text = window.location;
+      } else {
+        text = "You canceled!";
+      }
+      document.getElementById("confirm-message").innerHTML = text;
+ }
+
+ //set object in local storage
+ const addObject = () => {
+    const firstNameField = document.getElementById('first-name');
+    const lastNameField = document.getElementById('last-name');
+    const firstName = firstNameField.value;
+    const lastName = lastNameField.value;
+    
+    firstNameField.value = "";
+    lastNameField.value= "";
+    showName(firstName,lastName);
+    saveNameInLocalStorage(firstName,lastName);
+
+ }
+
+ const showName = (firstName, lastName) => {
+    const nameWrap = document.getElementById('name-conatiner');
+    const li = document.createElement('li');
+    li.innerText = `${firstName} ${lastName}`;
+    nameWrap.appendChild(li);
+ }
+ const getNameFromLocalStorage = () => {
+    let object = {};
+    const getObject = localStorage.getItem('object');
+    if (getObject) {
+        object = JSON.parse(getObject);
+    }
+    return object;
+ }
+
+ const saveNameInLocalStorage = (firstName, lastName) => {
+    const object = getNameFromLocalStorage();
+    object[firstName] = lastName;
+    const strinifyObject = JSON.stringify(object);
+    localStorage.setItem('object',strinifyObject);
+ }
